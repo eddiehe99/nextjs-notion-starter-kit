a0389ee45f817eca40d15dc506dae2f5a6f5dce1
Eddie He <erheng.he@m.scnu.edu.cn>
2025-07-28 22:53:13 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'eddiehe99',
   github: 'eddiehe99',
-  //   linkedin: 'fisch2',
+  linkedin: 'erheng-he-745282215/',
   mastodon: 'https://mastodon.world/@eddiehe99', // optional mastodon profile URL, provides link verification
   newsletter: 'https://paragraph.xyz/@eddiehe', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
